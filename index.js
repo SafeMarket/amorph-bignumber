9247833903ab6c6213d580a223f22602f405d45e
Aakil Fernandes <aakilfernandes@gmail.com>
2018-01-29 14:29:49 -0500

    signed/unsigned

--- index.js
+++ index.js
@@ -1,18 +1,69 @@
 const AmorphConverter = require('amorph-converter')
 const amorphHex = require('amorph-hex')
 const Bignumber = require('bignumber.js')
+const defunction = require('defunction')
+const getValidateInstanceOf = require('defunction/lib/validates/getInstanceOf')
+const getValidateConstructorNamed = require('defunction/lib/validates/getConstructorNamed')
+const getValidateAllOf = require('defunction/lib/validates/getAllOf')
 
-module.exports = new AmorphConverter((uint8Array) => {
+const validateUint8Array = getValidateInstanceOf(Uint8Array)
+const validateBignumber = getValidateConstructorNamed('BigNumber')
+const validateInteger = (label, arg) => {
+  if (!arg.mod(1).eq(0)) {
+    throw new Error(`${label} should be an integer, received ${arg}`)
+  }
+}
+const validateGteZero = (label, arg) => {
+  if (arg.lt(0)) {
+    throw new Error(`${label} should be greater than or equal to 0, received ${arg}`)
+  }
+}
+
+module.exports.unsigned = new AmorphConverter((uint8Array) => {
   if (uint8Array.length === 0) {
     return new Bignumber(0)
   }
   return new Bignumber(amorphHex.unprefixed.to(uint8Array), 16)
-}, (bignumber) => {
-  if (bignumber.eq(0)) {
-    return new Uint8Array(0)
+}, defunction(
+  [getValidateAllOf([validateBignumber, validateInteger, validateGteZero])],
+  validateUint8Array,
+  function from(bignumber) {
+    if (bignumber.eq(0)) {
+      return new Uint8Array(0)
+    }
+    return amorphHex.unprefixed.from(normalizeHex(bignumber.toString(16)))
   }
-  return amorphHex.unprefixed.from(normalizeHex(bignumber.toString(16)))
-})
+))
+
+module.exports.signed = new AmorphConverter((uint8Array) => {
+  if (uint8Array.length === 0) {
+    return new Bignumber(0)
+  }
+  const sign = (uint8Array[0] === 0) ? -1 : 1
+  const unsignedUint8Array = (sign === 1) ? uint8Array : uint8Array.slice(1)
+  if (unsignedUint8Array.length === 0) {
+    return new Bignumber(0)
+  }
+  return (new Bignumber(amorphHex.unprefixed.to(unsignedUint8Array), 16)).mul(sign)
+}, defunction(
+  [getValidateAllOf([validateBignumber, validateInteger])],
+  validateUint8Array,
+  function from(bignumber) {
+    if (bignumber.eq(0)) {
+      return new Uint8Array(0)
+    }
+    const bignumberAbs = bignumber.abs()
+    const unsignedUint8Array = amorphHex.unprefixed.from(normalizeHex(bignumberAbs.toString(16)))
+    if (bignumber.gt(0)) {
+      return unsignedUint8Array
+    }
+    const signedUint8Array = new Uint8Array(unsignedUint8Array.length + 1)
+    signedUint8Array[0] = 0
+    signedUint8Array.set(unsignedUint8Array, 1)
+    return signedUint8Array
+  }
+))
+
 
 function normalizeHex(hex) {
   return hex.length % 2 === 0 ? hex : '0' + hex
--- test.js
+++ test.js
@@ -1,25 +1,92 @@
-const amorphBignumber = require('./')
+const converters = require('./')
 const chai = require('chai')
 const Bignumber = require('bignumber.js')
 
 chai.should()
 
-describe('amorphBignumber', () => {
+describe('converters', () => {
 
-  it('should .to [1, 1]', () => {
-    amorphBignumber.to(new Uint8Array([1, 1])).toNumber().should.equal(257)
-  })
-  it('should .to []', () => {
-    amorphBignumber.to(new Uint8Array(0)).toNumber().should.equal(0)
-  })
-  it('should .to [0]', () => {
-    amorphBignumber.to(new Uint8Array([0])).toNumber().should.equal(0)
-  })
-  it('should .from 257', () => {
-    amorphBignumber.from(new Bignumber(257)).should.deep.equal(new Uint8Array([1, 1]))
-  })
-  it('should .from 0', () => {
-    amorphBignumber.from(new Bignumber(0)).should.deep.equal(new Uint8Array(0))
+  describe('unsigned', () => {
+    it('should .to [1, 1]', () => {
+      converters.unsigned.to(new Uint8Array([1, 1])).toNumber().should.equal(257)
+    })
+    it('should .to []', () => {
+      converters.unsigned.to(new Uint8Array(0)).toNumber().should.equal(0)
+    })
+    it('should .to [0]', () => {
+      converters.unsigned.to(new Uint8Array([0])).toNumber().should.equal(0)
+    })
+    it('should .from 257', () => {
+      converters.unsigned.from(new Bignumber(257)).should.deep.equal(new Uint8Array([1, 1]))
+    })
+    it('should .from 0', () => {
+      converters.unsigned.from(new Bignumber(0)).should.deep.equal(new Uint8Array(0))
+    })
+    it('should throw error when .from non-integer', () => {
+      let error
+      try {
+        converters.unsigned.from(new Bignumber(.5))
+      } catch (_error) {
+        error = _error
+      }
+      error.should.be.instanceof(Error)
+      error.message.should.equal('AmorphConverter.prototype.from:arguments[0] should be an integer, received 0.5')
+    })
+
+    it('should throw error when .from negative', () => {
+      let error
+      try {
+        converters.unsigned.from(new Bignumber(-1))
+      } catch (_error) {
+        error = _error
+      }
+      error.should.be.instanceof(Error)
+      error.message.should.equal('AmorphConverter.prototype.from:arguments[0] should be greater than or equal to 0, received -1')
+    })
   })
 
+  describe('signed', () => {
+    it('should .to [1, 1]', () => {
+      converters.signed.to(new Uint8Array([1, 1])).toNumber().should.equal(257)
+    })
+    it('should .to []', () => {
+      converters.signed.to(new Uint8Array(0)).toNumber().should.equal(0)
+    })
+    it('should .to [0]', () => {
+      converters.signed.to(new Uint8Array([0])).toNumber().should.equal(0)
+    })
+    it('should .from 257', () => {
+      converters.signed.from(new Bignumber(257)).should.deep.equal(new Uint8Array([1, 1]))
+    })
+    it('should .from 0', () => {
+      converters.signed.from(new Bignumber(0)).should.deep.equal(new Uint8Array(0))
+    })
+    it('should .from -1', () => {
+      converters.signed.from(new Bignumber(-1)).should.deep.equal(new Uint8Array([0, 1]))
+    })
+    it('should .from -257', () => {
+      converters.signed.from(new Bignumber(-257)).should.deep.equal(new Uint8Array([0, 1, 1]))
+    })
+    it('should throw error when .from non-integer', () => {
+      let error
+      try {
+        converters.signed.from(new Bignumber(.5))
+      } catch (_error) {
+        error = _error
+      }
+      error.should.be.instanceof(Error)
+      error.message.should.equal('AmorphConverter.prototype.from:arguments[0] should be an integer, received 0.5')
+    })
+
+    it('should throw error when .from negative non-integer', () => {
+      let error
+      try {
+        converters.signed.from(new Bignumber(-.5))
+      } catch (_error) {
+        error = _error
+      }
+      error.should.be.instanceof(Error)
+      error.message.should.equal('AmorphConverter.prototype.from:arguments[0] should be an integer, received -0.5')
+    })
+  })
 })
